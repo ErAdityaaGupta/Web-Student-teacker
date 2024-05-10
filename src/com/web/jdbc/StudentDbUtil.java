232605d6d9defcc14ad57049dcf53de679063098
ErAdityaaGupta <159624433+ErAdityaaGupta@users.noreply.github.com>
2024-05-11 01:40:42 +0530

    Update StudentDbUtil.java

--- src/com/web/jdbc/StudentDbUtil.java
+++ src/com/web/jdbc/StudentDbUtil.java
@@ -1,6 +1,7 @@
 package com.web.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -30,7 +31,7 @@ public class StudentDbUtil {
 			myConn = dataSource.getConnection();
 			
 			// create sql statement 
-			String sql = "Select * from students oder by last_name";
+			String sql = "SELECT * FROM student ORDER BY last_name";
 			
 			myStmt = myConn.createStatement();
 			
@@ -39,6 +40,7 @@ public class StudentDbUtil {
 			
 			// process result set
 			while(myRs.next()) {
+				
 				// retrive data from result set 
 				int id = myRs.getInt("id");
 				String firstName = myRs.getString("first_name");
@@ -50,14 +52,19 @@ public class StudentDbUtil {
 				
 				// add this object to the list of students
 				students.add(tempStudent);
+				
 			}
 			return students;
 		}
 		finally {
 			// close JDBC objects
 			close(myConn,myRs,myStmt);
+			
 		}
+		
+
 	}
+
 
 	private void close(Connection myConn, ResultSet myRs, Statement myStmt) {
 		try {
@@ -74,6 +81,120 @@ public class StudentDbUtil {
 		catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+
+	public void addStudent(Student theStudent) {
+		
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+		
+		try {
+			
+			//get db connection
+			myConn = dataSource.getConnection();
+			
+			//create sql for 
+			String sql = "INSERT INTO student" + "(first_name, last_name, email)" + "VALUES (?, ?, ?)";
+			myStmt = myConn.prepareStatement(sql);
+			//set the param values for the student
+			myStmt.setString(1, theStudent.getFirstName());
+			myStmt.setString(2, theStudent.getLastName());
+			myStmt.setString(3, theStudent.getEmail());
+			
+			//execute sql insert
+			myStmt.execute();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(myConn, null, myStmt);
+		}
+	}
+
+
+	public Student getStudent(String theStudentId) {
+		
+		Student theStudent = null;
+		
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+		ResultSet myRs = null;
+		
+		int studentId;
+		
+		try {
+			// convert student id to int
+			studentId = Integer.parseInt(theStudentId);
+
+			// get connection to database
+			myConn = dataSource.getConnection();
+
+			// create sql to get selected student
+			String sql = "SELECT * FROM student WHERE id=?";
+
+			// create prepared statement
+			myStmt = myConn.prepareStatement(sql);
+
+			// set params
+			myStmt.setInt(1, studentId);
+
+			// execute statement
+			myRs = myStmt.executeQuery();
+
+			// retrieve data from result set row
+			if (myRs.next()) {
+				String firstName = myRs.getString("first_name");
+				String lastName = myRs.getString("last_name");
+				String email = myRs.getString("email");
+
+				// use the studentId during construction
+				theStudent = new Student(studentId, firstName, lastName, email);
+			} else {
+				throw new Exception("Could not find student id: " + studentId);
+			}
+
+			return theStudent;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(myConn, myRs, myStmt);
+		}
+		
+		
+		return theStudent;
+	}
+
+
+	public void updateStudent(Student theStudent) {
+
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+
+		try {
+			// get db connection
+			myConn = dataSource.getConnection();
+
+			// create sql update statement
+			String sql = "UPDATE student " + "SET first_name=?, last_name=?, email=? " + "WHERE id=?";
+
+			// prepare statement
+			myStmt = myConn.prepareStatement(sql);
+
+			// set params
+			myStmt.setString(1, theStudent.getFirstName());
+			myStmt.setString(2, theStudent.getLastName());
+			myStmt.setString(3, theStudent.getEmail());
+			myStmt.setInt(4, theStudent.getId());
+
+			// execute sql statement
+			myStmt.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(myConn, null, myStmt);
+		}
+		
 	}
 	
 	
